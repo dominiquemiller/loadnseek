136c24f6db6099765bdeb390fb65e2283b1dadc4
Dominique Miller <dominique.p.miller@gmail.com>
2015-12-07 19:32:19 -0500

    removed standard rails comments prt 2

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,14 +1,10 @@
 class PhotosController < ApplicationController
   before_action :set_photo, only: [:show, :edit, :update, :destroy]
 
-  # GET /photos
-  # GET /photos.json
   def index
     @photos = Photo.search(params[:keyword])
   end
 
-  # GET /photos/1
-  # GET /photos/1.json
   def show
     respond_to do |format|
       format.html
@@ -16,18 +12,13 @@ class PhotosController < ApplicationController
     end
   end
 
-
-  # GET /photos/new
   def new
     @photo = Photo.new
   end
 
-  # GET /photos/1/edit
   def edit
   end
 
-  # POST /photos
-  # POST /photos.json
   def create
 
     @photo = current_user.photos.create(photo_params)
@@ -43,8 +34,6 @@ class PhotosController < ApplicationController
     end
   end
 
-  # PATCH/PUT /photos/1
-  # PATCH/PUT /photos/1.json
   def update
     respond_to do |format|
       if @photo.update(photo_params)
@@ -57,8 +46,6 @@ class PhotosController < ApplicationController
     end
   end
 
-  # DELETE /photos/1
-  # DELETE /photos/1.json
   def destroy
     @photo.destroy
     respond_to do |format|
@@ -68,12 +55,10 @@ class PhotosController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_photo
       @photo = Photo.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def photo_params
       params.require(:photo).permit(:title, :picture, :user_id, :url)
     end
